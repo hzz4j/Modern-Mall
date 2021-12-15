ac4f3a2faa2deef4589abf8201adea6e6a36e83e
静默 <cau1403090523@gmail.com>
2021-12-15 13:17:04 +0800

    聚合工程创建

---
+++ modern-mall-backend/mall-admin/src/test/java/org/hzz/malladmin/MallAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hzz.malladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
